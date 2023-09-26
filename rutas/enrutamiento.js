0acc225fb7fa41968df01a3add34bb3dd2023f61
Pamela Ospina <pospina3@soy.sena.edu.co>
2023-09-26 15:11:15 -0500

    Cambios en formulario de registro

--- rutas/enrutamiento.js
+++ rutas/enrutamiento.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/', vistas.index)
 router.get('/localidades', vistas.localidades)
+router.get('/formularioRegistro', vistas.formularioRegistroUsuario)
 
 module.exports = router
